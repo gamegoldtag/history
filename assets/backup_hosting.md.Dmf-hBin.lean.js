eb7d7b4293b8dfcf2ada8991fef07df4d31e5cfd
ian.lab(이재원)/melon <ian.lab@kakaoent.com>
2024-10-23 16:49:29 +0900

    커밋

---
+++ assets/backup_hosting.md.Dmf-hBin.lean.js
@@ -0,0 +1,20 @@
+import{_ as e,c as o,a0 as r,o as t}from"./chunks/framework.CGHvQLJz.js";const b=JSON.parse('{"title":"cafe24 호스팅 정보","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"backup/hosting.md","filePath":"backup/hosting.md"}'),n={name:"backup/hosting.md"};function i(d,a,c,h,s,p){return t(),o("div",null,a[0]||(a[0]=[r(`<h1 id="cafe24-호스팅-정보" tabindex="-1">cafe24 호스팅 정보 <a class="header-anchor" href="#cafe24-호스팅-정보" aria-label="Permalink to &quot;cafe24 호스팅 정보&quot;">​</a></h1><p>도메인 업체 정보 및 호스팅 서버 정보</p><h2 id="도메인-정보" tabindex="-1">도메인 정보 <a class="header-anchor" href="#도메인-정보" aria-label="Permalink to &quot;도메인 정보&quot;">​</a></h2><h3 id="가비아-gabia" tabindex="-1">가비아 gabia <a class="header-anchor" href="#가비아-gabia" aria-label="Permalink to &quot;가비아 gabia&quot;">​</a></h3><pre><code>- https://www.gabia.com/
+- pjwpcw
+- v3rpxsk3
+</code></pre><h2 id="호스팅-정보" tabindex="-1">호스팅 정보 <a class="header-anchor" href="#호스팅-정보" aria-label="Permalink to &quot;호스팅 정보&quot;">​</a></h2><h3 id="카페-24-사이트-정보" tabindex="-1">카페 24 사이트 정보 <a class="header-anchor" href="#카페-24-사이트-정보" aria-label="Permalink to &quot;카페 24 사이트 정보&quot;">​</a></h3><pre><code>- https://hosting.cafe24.com/
+- pch14
+- bigsnow2023!!
+</code></pre><h3 id="카페24-호스팅-접속-ssh" tabindex="-1">카페24 호스팅 접속 (SSH) <a class="header-anchor" href="#카페24-호스팅-접속-ssh" aria-label="Permalink to &quot;카페24 호스팅 접속 (SSH)&quot;">​</a></h3><pre><code>- 210.114.17.212: 22
+- id: root
+- pw: spingsnow0405!!
+</code></pre><h3 id="카페24-호스팅-접속-ftp" tabindex="-1">카페24 호스팅 접속 (FTP) <a class="header-anchor" href="#카페24-호스팅-접속-ftp" aria-label="Permalink to &quot;카페24 호스팅 접속 (FTP)&quot;">​</a></h3><pre><code>- 210.114.17.212: 2121
+- id: root
+- pw: spingsnow0405!!
+</code></pre><h3 id="카페24-호스팅-접속-fpt" tabindex="-1">카페24 호스팅 접속 (FPT) <a class="header-anchor" href="#카페24-호스팅-접속-fpt" aria-label="Permalink to &quot;카페24 호스팅 접속 (FPT)&quot;">​</a></h3><pre><code>- 210.114.17.212
+- id : ftpadmin
+- pw : spingsnow1234!!
+</code></pre><h3 id="카페24-호스팅-접속-mariadb" tabindex="-1">카페24 호스팅 접속 (MariaDB) <a class="header-anchor" href="#카페24-호스팅-접속-mariadb" aria-label="Permalink to &quot;카페24 호스팅 접속 (MariaDB)&quot;">​</a></h3><pre><code>- host : 210.114.17.212:3306
+- id: kmac
+- pw: voc1234!@#$
+- db명 : kmacdb
+</code></pre>`,16)]))}const f=e(n,[["render",i]]);export{b as __pageData,f as default};
